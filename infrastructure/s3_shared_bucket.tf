6089346ffaee7b6671895ae0cf1ba9946133ffae
Berenice Cotero <berenice.cotero@clarivate.com>
2018-04-03 14:51:58 -0400

    added lifecycle snapshots prefix

--- infrastructure/s3_shared_bucket.tf
+++ infrastructure/s3_shared_bucket.tf
@@ -9,25 +9,8 @@ variable user               { }
 variable app                { }
 variable aws_wos_dev        { }
 variable aws_wos_prod       { }
+variable snapshots_prefix   { }
 variable bucket_policy_tmpl { }
-
-resource "aws_s3_bucket" "build-tools" {
-  bucket = "clarivate.${var.app}.${var.env}.${var.region}.build-tools" 
-  acl = "private"
-  force_destroy = true
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-  tags {
-    Owner       = "WOS-DevOps"
-    Environment = "${var.env}"
-    User        = "${var.user}"
-  }
-}
 
 data "template_file" "bucket_policy" {
   template = "${ file( "${ var.bucket_policy_tmpl }" ) }"
@@ -35,6 +18,41 @@ data "template_file" "bucket_policy" {
     bucket_name      = "${aws_s3_bucket.build-tools.id}"
     aws_account_dev  = "${var.aws_wos_dev}"
     aws_account_prod = "${var.aws_wos_prod}"
+  }
+}
+
+resource "aws_s3_bucket" "build-tools" {
+  bucket = "clarivate.${var.app}.${var.env}.${var.region}.build-tools" 
+  acl = "private"
+  force_destroy = true
+
+  lifecycle_rule {
+    id      = "snapshots"
+    prefix  = "${var.snapshots_prefix}"
+    enabled = true
+
+    tags {
+      "rule"       = "snapshots"
+      "clean_days" = "14-days"
+    }
+
+    expiration {
+      days = 14
+    }
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags {
+    Owner       = "WOS-DevOps"
+    Environment = "${var.env}"
+    User        = "${var.user}"
   }
 }
 
--- infrastructure/terraform.tfvars
+++ infrastructure/terraform.tfvars
@@ -2,9 +2,10 @@
 #           BOOTSTRAP VARIABLES
 # ----------------------------------------
 
-region       = "us-east-1"
-env          = "dev"
-app          = "wos"
-aws_wos_dev  = "078897461510"
-aws_wos_prod = "996733901146"
+region             = "us-east-1"
+env                = "dev"
+app                = "wos"
+aws_wos_dev        = "078897461510"
+aws_wos_prod       = "996733901146"
+snapshots_prefix   = "/artifacts/snapshots/"
 bucket_policy_tmpl = "template_files/shared_bucket_policy.tmpl"
